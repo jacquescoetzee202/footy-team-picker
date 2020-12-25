b0661f6e338183568f8a54e6abe15f2f671c0d35
Jacques <jacquescoetzee91@gmail.com>
2020-12-25 12:38:59 +0000

    defined initial redux state

---
+++ src/data/initial.js
@@ -0,0 +1,15 @@
+const initial = {
+    dark_mode: false,
+    detailed: false,
+    team1: {
+        name: "",
+        kit_color: 1
+    },
+    team2: {
+        name: "",
+        kit_color: 2
+    },
+    players: [],
+};
+
+export default initial;
