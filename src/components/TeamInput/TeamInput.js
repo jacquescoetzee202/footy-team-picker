f74bc99bdc4e66222494ba65e27c33ff49e1405f
Jacques <jacquescoetzee91@gmail.com>
2020-12-26 09:06:18 +0000

    added form text input to TeamInput

--- src/components/TeamInput/TeamInput.js
+++ src/components/TeamInput/TeamInput.js
@@ -1,6 +1,15 @@
 const TeamInput = () => (
     <div>
         <h1>Team 1</h1>
+        <form>
+            <label htmlFor="team-name">Name:</label>
+            <input
+                id="team-name"
+                type="text"
+            >
+            </input>
+            <button>Add team</button>
+        </form>
     </div>
 );
 
