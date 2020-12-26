5958c3f6b46bf125e9b3ecd37b3dde80c0f29042
Jacques <jacquescoetzee91@gmail.com>
2020-12-26 12:02:09 +0000

    installed react-router-dom and made routes for set-up/team1 and set-up/team2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import './styles/index.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import TeamInput from "./components/TeamInput";
 
 function App() {
   return (
-    <div className="App">
-      <h1>footy team picker!</h1>
-      <TeamInput />
-    </div>
+    <Router>
+      <header>
+        <h1>footy team picker!</h1>
+      </header>
+      <Route exact path="/set-up/team1">
+        <TeamInput />
+      </Route>
+      <Route exact path="/set-up/team2">
+        <TeamInput />
+      </Route>
+    </Router>
   );
 }
 
