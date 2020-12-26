41a57a7d8c7ad08d406d9f56a21739b96b88806e
Jacques <jacquescoetzee91@gmail.com>
2020-12-26 09:28:12 +0000

    updated TeamInput to class based component

--- src/components/TeamInput/TeamInput.js
+++ src/components/TeamInput/TeamInput.js
@@ -1,19 +1,33 @@
 import { CirclePicker } from "react-color";
+import { Component } from "react";
 
-const TeamInput = () => (
-    <div>
-        <h1>Team 1</h1>
-        <form>
-            <label htmlFor="team-name">Name:</label>
-            <input
-                id="team-name"
-                type="text"
-            >
-            </input>
-            <button>Add team</button>
-        </form>
-        <CirclePicker/>
-    </div>
-);
+class TeamInput extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            name:"",
+            color:"",
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Team 1</h1>
+                <form>
+                    <label htmlFor="team-name">Name:</label>
+                    <input
+                        id="team-name"
+                        type="text"
+                    >
+                    </input>
+                    <button>Add team</button>
+                </form>
+                <CirclePicker/>
+            </div>
+        );
+    }
+}
 
 export default TeamInput;
