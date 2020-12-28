2cb3746d378327f4ceb7f0ad5805432aa2764fea
Jacques <jacquescoetzee91@gmail.com>
2020-12-28 09:17:10 +0000

    set up buttonGroup component to accept props for onClick handler and css styling for selected button

--- src/components/ButtonGroup/ButtonGroup.js
+++ src/components/ButtonGroup/ButtonGroup.js
@@ -1,9 +1,27 @@
-const ButtonGroup = () => {
+const ButtonGroup = ({ ratingClick, selected }) => {
     return(
         <div className="btn-group">
-            <button>⭐</button>
-            <button>⭐⭐</button>
-            <button>⭐⭐⭐</button>
+            <button 
+                onClick={ ratingClick }
+                data-rating={ 1 }
+                className={ selected === 1 ? "selected" : "" }
+            >
+                ⭐
+            </button>
+            <button 
+                onClick={ ratingClick }
+                data-rating={ 2 }
+                className={ selected === 2 ? "selected" : "" }
+            >
+                ⭐⭐
+            </button>
+            <button 
+                onClick={ ratingClick }
+                data-rating={ 3 }
+                className={ selected === 3 ? "selected" : "" }
+            >
+                ⭐⭐⭐
+            </button>
         </div>
     );
 };
