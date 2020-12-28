0ffbbbd7adf14733eb360b35773fd1068a22cb71
Jacques <jacquescoetzee91@gmail.com>
2020-12-28 14:31:26 +0000

    mapStateToProps for player entries and team name and kit color

--- src/components/SelectedTeam/index.js
+++ src/components/SelectedTeam/index.js
@@ -1,3 +1,16 @@
 import SelectedTeam from "./SelectedTeam";
 
-export default SelectedTeam;
+import { connect } from "react-redux";
+
+// import { incrementPlayerNum, savePlayers } from "../../data/actions/state";
+
+const mapStateToProps = ( state ) => {
+    let { players, team1, team2 } = state;
+    return {
+        players,
+        team1,
+        team2,
+    }
+}
+
+export default connect( mapStateToProps )(SelectedTeam);
