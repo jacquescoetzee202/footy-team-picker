c5758c95ed6c2452ef59bec0f300286944f1f5b0
Jacques <jacquescoetzee91@gmail.com>
2020-12-29 16:23:23 +0000

    created routing on start button press to navigate to team1 input page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,23 @@
 import { Component } from "react";
 
+import history from "../../history";
+
 class Home extends Component {
+    constructor() {
+        super()
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        history.push("/set-up/team1");
+    }
+
     render() {
         return (
             <>
                 <p>Some info on the start page</p>
-                <button>Start</button>
+                <button onClick={ this.handleClick }>Start</button>
             </>
         );
     }
