dcd929b098f997d1b51d681289f581dc4c4a18f9
Jacques <jacquescoetzee91@gmail.com>
2021-01-02 18:13:11 +0000

    added header svg's

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,7 @@
 import { ReactComponent as Ball } from "../assets/svg/ball.svg";
+import { ReactComponent as GitHub } from "../assets/svg/github.svg";
+import { ReactComponent as Home } from "../assets/svg/home.svg";
+import { ReactComponent as Reset } from "../assets/svg/reset.svg";
 
 const Header = () => (
     <header className="header">
@@ -7,7 +10,9 @@ const Header = () => (
             <h1>team picker</h1>
         </div>
         <div className="header__links">
-            <p>links</p>
+            <GitHub/>
+            <Home />
+            <Reset />
         </div>
     </header>
 )
