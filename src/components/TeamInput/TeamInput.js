e5456b054cd129ad620d50c9839dfd40d0cb4835
Jacques <jacquescoetzee91@gmail.com>
2021-01-03 08:52:50 +0000

    added teaminput styling with kit svg

--- src/components/TeamInput/TeamInput.js
+++ src/components/TeamInput/TeamInput.js
@@ -1,6 +1,8 @@
 import { CirclePicker } from "react-color";
 import { Component } from "react";
 import history from "../../history";
+import { ReactComponent as KitFill } from "../../assets/svg/shirt-fill.svg";
+import { ReactComponent as Kit } from "../../assets/svg/shirt.svg";
 
 class TeamInput extends Component {
     constructor(props) {
@@ -46,7 +48,7 @@ class TeamInput extends Component {
         let filledIn = this.props.filledIn;
 
         return(
-            <div>
+            <div className="team-card card">
                 <h2>Team { this.props.teamInput }</h2>
                 <form onSubmit={ this.handleSubmit }>
                     <label htmlFor="team-name">Name:</label>
@@ -57,8 +59,11 @@ class TeamInput extends Component {
                         required
                     >
                     </input>
-                    <CirclePicker color={ this.state.color } onChange={ this.colorChange }/>
-                    <button>{ filledIn ? "update" : "add team" }</button>
+                    <div className="team-card__color card__sub">
+                        <CirclePicker color={ this.state.color } onChange={ this.colorChange }/>
+                        <KitFill />
+                    </div>
+                    <button className="button">{ filledIn ? "update" : "add team" }</button>
                 </form>
             </div>
         );
