8fcd7b3ea70a34cb8af44397d47ea9b9d51cad52
Jacques <jacquescoetzee91@gmail.com>
2021-01-03 16:25:34 +0000

    added next and prev button styling to disable when form input not complete

--- src/components/NavArrows/NavArrows.js
+++ src/components/NavArrows/NavArrows.js
@@ -2,7 +2,7 @@ import { Component } from "react";
 
 import { pageNames } from "../../data/routes";
 
-import { ReactComponent as Next } from "../../assets/svg/next-page-filled.svg";
+import Next from "../Next";
 
 class NavArrows extends Component {
     
@@ -11,6 +11,7 @@ class NavArrows extends Component {
 
         this.nextClick = this.nextClick.bind(this);
         this.prevClick = this.prevClick.bind(this);
+        this.nextArrowState = this.nextArrowState.bind(this);
     }
     
     nextClick() {
@@ -22,20 +23,39 @@ class NavArrows extends Component {
         console.log("prev");
         this.props.navArrowClick(-1);
     }
+
+    nextArrowState(currentStage, progress) {
+        if( currentStage === 1 ){
+            return true;
+        }
+        if( currentStage === 2 ){
+            return progress.team1;
+        }
+        if( currentStage === 3 ){
+            return progress.team2;
+        }
+        if( currentStage === 4 ){
+            return progress.players;
+        }
+        if( currentStage === 5 ){
+            return false;
+        }
+    }
     
     render() {
-        let currentPage = pageNames[ this.props.currentStage -1 ];
+        let { currentStage, progress } = this.props;
+        let currentPage = pageNames[ currentStage -1 ];
 
         return (
             <div className="nav-arrows">
-                <button onClick={ this.prevClick } disabled={ this.props.currentStage === 1 }>
-                    <Next className="nav-arrows__prev"/>
+                <button className="nav-arrows__button" onClick={ this.prevClick } disabled={ currentStage === 1 }>
+                    <Next css={`nav-arrows__prev ${ currentStage === 1 ? "disabled-arrow" : ""}`}/>
                 </button>
                 <div className="nav-arrows__text-box">
                     <h2 className="nav-arrows__text-box__text">{ currentPage }</h2>
                 </div>
-                <button onClick={ this.nextClick } disabled={ this.props.currentStage === 5 }>
-                    <Next className="nav-arrows__next"/>
+                <button className="nav-arrows__button" onClick={ this.nextClick } disabled={ !this.nextArrowState( currentStage, progress ) }>
+                    <Next css={`nav-arrows__next ${!this.nextArrowState( currentStage, progress ) ? "disabled-arrow" : ""}`}/>
                 </button>
             </div>
         );
--- src/components/NavArrows/index.js
+++ src/components/NavArrows/index.js
@@ -5,8 +5,10 @@ import { connect } from "react-redux";
 import { navArrowInc } from "../../data/actions/state";
 
 const mapStateToProps = ( state ) => {
+
     return {
         currentStage: +state.currentStage,
+        progress: state.progress,
     }
 }
 
