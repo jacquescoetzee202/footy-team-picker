b69ba0d44713214647d4a4a7cce6b9da18e424c1
Jacques <jacquescoetzee91@gmail.com>
2021-01-03 12:41:08 +0000

    completed button-group styling

--- src/components/ButtonGroup/ButtonGroup.js
+++ src/components/ButtonGroup/ButtonGroup.js
@@ -2,33 +2,35 @@ const ButtonGroup = ({ handleClick, selected, id, skill }) => {
     return(
         <div className="btn-group">
             <div className="btn-group__label"><p>{ skill }</p></div>
-            <button 
-                onClick={ handleClick }
-                data-rating={ 1 }
-                data-id={ id }
-                data-skill={ skill }
-                className={`btn-group__btn ${ selected === 1 ? "selected" : "" }`}
-            >
-                ⭐
-            </button>
-            <button 
-                onClick={ handleClick }
-                data-rating={ 2 }
-                data-id={ id }
-                data-skill={ skill }
-                className={`btn-group__btn ${ selected === 2 ? "selected" : "" }`}
-            >
-                ⭐⭐
-            </button>
-            <button 
-                onClick={ handleClick }
-                data-rating={ 3 }
-                data-id={ id }
-                data-skill={ skill }
-                className={`btn-group__btn ${ selected === 3 ? "selected" : "" }`}
-            >
-                ⭐⭐⭐
-            </button>
+            <div className="btn-group__btns">
+                <button 
+                    onClick={ handleClick }
+                    data-rating={ 1 }
+                    data-id={ id }
+                    data-skill={ skill }
+                    className={`btn-group__btns__btn left-btn ${ selected === 1 ? "selected" : "" }`}
+                >
+                    ⭐
+                </button>
+                <button 
+                    onClick={ handleClick }
+                    data-rating={ 2 }
+                    data-id={ id }
+                    data-skill={ skill }
+                    className={`btn-group__btns__btn ${ selected === 2 ? "selected" : "" }`}
+                >
+                    ⭐⭐
+                </button>
+                <button 
+                    onClick={ handleClick }
+                    data-rating={ 3 }
+                    data-id={ id }
+                    data-skill={ skill }
+                    className={`btn-group__btns__btn right-btn ${ selected === 3 ? "selected" : "" }`}
+                >
+                    ⭐⭐⭐
+                </button>
+            </div>
         </div>
     );
 };
